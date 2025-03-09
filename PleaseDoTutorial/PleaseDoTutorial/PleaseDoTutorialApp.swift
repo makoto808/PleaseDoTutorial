fdcf007f69e000f9a342d716bcd4e875c7b11df0
Gregg Abe <greggabe808@gmail.com>
2025-03-09 00:12:46 -1000

    Add project to repo.

---
+++ PleaseDoTutorial/PleaseDoTutorial/PleaseDoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoTutorialApp.swift
+//  PleaseDoTutorial
+//
+//  Created by Gregg Abe on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
