2b8bb41f40db86acbca26911adfd130aab351458
Gregg Abe <greggabe808@gmail.com>
2025-03-19 15:25:26 -1000

    Added func signIn to LoginManager.

--- PleaseDoTutorial/App/LoginManager.swift
+++ PleaseDoTutorial/App/LoginManager.swift
@@ -12,7 +12,16 @@ import FirebaseFirestore
 final class LoginManager {
     
     private let db = Firestore.firestore()
+    private var handler: AuthStateDidChangeListenerHandle?
     var currentUser: User?
+    
+    init() {
+        setupListener()
+    }
+    
+    deinit {
+        removeListener()
+    }
     
     func signUp(_ fname: String, _ lname: String, _ email: String, _ pw: String) {
         Task {
@@ -27,9 +36,48 @@ final class LoginManager {
                     "id": id
                 ])
                 print("Document successfully written!")
+                setupListener()
             } catch {
                 print(error)
             }
+        }
+    }
+    
+    func signIn( _ email: String, _ pw: String) {
+        Task {
+            do {
+                let result =  try await Auth.auth().signIn(withEmail: email, password: pw)
+                currentUser = result.user
+                print("Successfully signed in user!")
+                setupListener()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            removeListener()
+            print("Successfully signed out user!")
+        } catch {
+            print(error)
+        }
+    }
+    
+    func setupListener() {
+        guard handler == nil else { return }
+        handler = Auth.auth().addStateDidChangeListener { [weak self] auth, user in
+            guard let self else { return }
+            currentUser = auth.currentUser
+            
+        }
+    }
+    
+    func removeListener() {
+        if let h = handler {
+            Auth.auth().removeStateDidChangeListener(h)
         }
     }
 }
