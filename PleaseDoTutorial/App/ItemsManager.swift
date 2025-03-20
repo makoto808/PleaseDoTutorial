2d097d1b3e892b0e280b092a276db8d0ee9e4c08
Gregg Abe <greggabe808@gmail.com>
2025-03-19 16:24:09 -1000

    Created ItemsManager.

---
+++ PleaseDoTutorial/App/ItemsManager.swift
@@ -0,0 +1,15 @@
+//
+//  ItemsManager.swift
+//  PleaseDoTutorial
+//
+//  Created by Gregg Abe on 3/19/25.
+//
+
+import Foundation
+
+final class ItemsManager {
+    static let shared = ItemsManager()
+    private init() {}
+    
+    
+}
